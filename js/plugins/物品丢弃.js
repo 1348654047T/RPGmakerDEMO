4ec082d9e91751c997d84b50112a005982d5ce5f
1348654047@qq.com <89444140+1348654047T@users.noreply.github.com>
2021-08-24 21:07:11 +0800

    Revert "kancolle"
    This reverts commit 5dc7d46c252650d9b662b74f933baac1c2933baa.

--- js/plugins/物品丢弃.js
+++
@@ -1,6 +0,0 @@
-$gameParty.gainItem($dataItems[id], num);  //丢物品
-$gameParty.gainItem($dataWeapons[id], num, includeEquip);  //丢武器
-$gameParty.gainItem($dataArmors[id], num, includeEquip);  //丢护甲
-// id: 要处理的物品序号；
-// num: 处理的物品数量，获取用正整数，丢弃用负整数；
-// includeEquip: 是否处理已装备的物品，0 - 不处理，1 - 处理。
